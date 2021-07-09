d0fda93c490f9ec8698d2c456c6cc2d304f511c9
Jonathon Brunson <jonathonbrunson21@gmail.com>
2021-07-08 21:09:00 -0500

    feat: add polyline and polygon exporters

---
+++ src/shapes/polygon.py
@@ -0,0 +1,66 @@
+from .shape_types import TS, SHP_TYPES
+from ..helpers import _read_little_int, _read_big_int, _read_little_double, _read_big_double, _read_bounding_box
+from .shape import Shape
+
+class Polygon(Shape):
+
+    shape_type = 5
+    shape_desc = "Polygon"
+
+    def __init__(self, idx, points, parts, bbox):
+        Shape.__init__(self)
+        self.idx    = idx
+        self.points = points
+        self.parts  = parts
+        self.npoints = len(points)
+        self.nparts  = len(parts)
+        self.bbox = bbox
+        
+    # TODO: REMOVE BUGS LATER
+    @staticmethod
+    def read(b, verbose = False):
+        if verbose: print("reading polygon...")
+        
+        # 0-3 	int32 	big 	Record number (1-based)
+        idx = _read_big_int(b)
+        if verbose: print("  idx: %d"%idx)
+
+        # 4-7 	int32 	big 	Record length (in 16-bit words)
+        length = _read_big_int(b) * 2
+        if verbose: print("  length: %d [bytes]"%length)
+        
+        # 0-3 	int32 	little 	Shape type (see reference below)
+        shape_type = _read_little_int(b)
+        shape_desc = SHP_TYPES[shape_type]
+        assert (shape_type == 5)
+        if verbose: print("  shape type: %d   desc: %s"%(shape_type, shape_desc))
+        
+        # 4 doubles with bounding box
+        xmin, xmax, ymin, ymax = _read_bounding_box(b)
+        bbox = (xmin, xmax, ymin, ymax)
+        if verbose: print( "  (xmin, xmax, ymin, ymax): (%g,%g,%g,%g)"%bbox)
+        
+        #
+        numParts  = _read_little_int(b)
+        numPoints = _read_little_int(b)
+        if verbose: print("  #parts: %d  #points: %d"%(numParts, numPoints))
+
+        parts = []
+        for i in range(numParts):
+            p = _read_little_int(b)
+            if verbose: print("  parts[i]: %d"%(p))
+            parts.append(b)
+
+        points = []
+        for i in range(numPoints):
+            x = _read_little_double(b)
+            y = _read_little_double(b)
+            if verbose: print("  (x,y): (%g,%g)"%(x,y))
+            points.append((x,y))
+
+        return Polygon(idx, points, parts, bbox)
+        
+    def __str__(self):
+        s = "Polygon[%d]\n"%self.idx
+        s = s + Shape.__str__(self)
+        return s
